3f465a094d6c1774471439ba643e955060e6e9c8
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-07 21:01:26 +0530

    Created AppController As a Singleton And Created Article Model
    Register with newsapi.org and get api and endpoints from their

---
+++ app/app/src/main/java/com/example/north/data/Article.java
@@ -0,0 +1,58 @@
+package com.example.north.data;
+
+public class Article {
+    private String authore;
+    private String title;
+    private String description;
+    private String newsUrl;
+    private String imageUrl;
+    private String publishedDate;
+
+    public String getAuthore() {
+        return authore;
+    }
+
+    public void setAuthore(String authore) {
+        this.authore = authore;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNewsUrl() {
+        return newsUrl;
+    }
+
+    public void setNewsUrl(String newsUrl) {
+        this.newsUrl = newsUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+}

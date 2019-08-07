bd1a64ad778a1445cd0793fe71f393ed4b4ebe64
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-07 21:57:05 +0530

    Creating Model And Add To ArrayList

--- app/app/src/main/java/com/example/north/data/ArticleData.java
+++ app/app/src/main/java/com/example/north/data/ArticleData.java
@@ -10,6 +10,8 @@ import com.example.north.controller.AppController;
 import com.example.north.misc.Config;
 import com.example.north.model.Article;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -22,7 +24,29 @@ public class ArticleData {
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                
+                try {
+                    JSONArray articleArray = response.getJSONArray("articles");
+                    for (int i=0; i<articleArray.length(); i++) {
+
+                        // Create Article Object
+                        JSONObject articleObject = articleArray.getJSONObject(i);
+                        Article article = new Article();
+                        article.setAuthore(articleObject.getString("author"));
+                        article.setDescription(articleObject.getString("description"));
+                        article.setTitle(articleObject.getString("title"));
+                        article.setImageUrl(articleObject.getString("url"));
+                        article.setImageUrl(articleObject.getString("urlToImage"));
+                        article.setPublishedDate(articleObject.getString("publishedAt"));
+
+
+                        articles.add(article);
+                    }
+
+                    Log.v("Articles Object: ", articles.toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
